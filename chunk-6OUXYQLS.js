369e899aee713deb816a747a87fd4155a57e5d85
AmberLilith <amber.lilith.ordone@gmail.com>
2026-03-15 09:28:28 -0300

    Auto-generated commit

---
+++ chunk-6OUXYQLS.js
@@ -0,0 +1,1 @@
+import"./chunk-SIAVTO45.js";var t=[{path:"login",loadComponent:()=>import("./chunk-CRCPZU2V.js").then(o=>o.LoginComponent)},{path:"",redirectTo:"login",pathMatch:"full"}];export{t as AUTH_ROUTES};
