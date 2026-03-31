c96de8d4b6e4d129f034b4b570dd4c2440e1b7d7
AmberLilith <amber.lilith.ordone@gmail.com>
2026-03-31 18:55:38 -0300

    Add gitignore environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,4 +2,5 @@ export const environment = {
   production: false,
   supabaseUrl: 'http://localhost:8000',
   supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJhbm9uIiwKICAgICJpc3MiOiAic3VwYWJhc2UtZGVtbyIsCiAgICAiaWF0IjogMTY0MTc2OTIwMCwKICAgICJleHAiOiAxNzk5NTM1NjAwCn0.dc_X5iR_VP_qT0zsiyj_I_OZ2T9FtRU2BBNWN8Bu4GE' ,// a mesma do .env
-  cryptoKey: '8f2kL9#mPq4vX7*zR2nT5bW9cE1sY6xJ'};
+  cryptoKey: '8f2kL9#mPq4vX7*zR2nT5bW9cE1sY6xJ'
+};
